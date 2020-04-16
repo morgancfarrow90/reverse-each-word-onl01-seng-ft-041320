e43632cd80d24830a6cf6b4c3c8385512bf26f7e
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-16 10:57:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
   split_string = string.split(" ")
   newarray = []
-  newarray.each do |word|
-    word.reverse
+  split_string.each do |word|
+    newarray << word.reverse()
   end
   backwardstring
 end
