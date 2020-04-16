46ef57952e164de8e49e9782c6fe119a331f40be
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-16 12:07:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,8 @@
 def reverse_each_word(string)
- string = string.reverse('')
- reversed_string = []
- 
- string.collect do |char|
-    reversed_string.unshift(char)
- end
- 
- return reversed_string.join('')
+  array = string.split(" ") #turn string into an array
+  test_array = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse #reverse each word in the array
+  end
+  test_array.join(" ")
 end
